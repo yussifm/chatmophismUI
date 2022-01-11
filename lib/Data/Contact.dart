5499b78d2efe74b9b8a87a22ae85fc803398b0c9
yussifm13 <yussifm13@gmail.com>
2022-01-11 03:30:20 +0000

    fix

--- lib/Data/Contact.dart
+++ lib/Data/Contact.dart
@@ -2,7 +2,7 @@ List<Map> allContacts = [
   {"name": "Mohammed", "message": "Am coming", "time": "yesteday"},
   {"name": "Coded", "message": "How is 👨🏾‍💻👩🏾‍💻", "time": "now"},
   {"name": "Alex", "message": "Am working 🏋🏾‍♂️", "time": "yesteday"},
-  {"name": "💖", "message": "How far✋🏾", "time": "1:05"},
+  {"name": "My💖", "message": "How far✋🏾", "time": "1:05"},
   {"name": "Girl11", "message": "Love you too 💖", "time": "yesteday"},
   {"name": "coded", "message": "Hello 🖐🏾", "time": "12:45"},
   {"name": "BBe", "message": "Am coming 🚗", "time": "friday"},
